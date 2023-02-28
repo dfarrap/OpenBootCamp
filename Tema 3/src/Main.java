0d004c80d975d6e7e6c9c70c1c275797ac29e08e
Diego Fernando Aguilar Restrepo <DiegoAR@unisabana.edu.co>
2023-02-28 10:48:18 -0500

    Primer commit

---
+++ Tema 3/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*PRIMERA PARTE
+        Crear una función con tres parámetros que sean números que se suman entre sí.
+        *Llamar a la función en el main y darle valores.*/
+
+        int a = suma(1, 2, 3);
+        System.out.println("suma:" + a);
+
+
+        /*SEGUNDA PARTE*/
+        Coche esteCoche = new Coche();
+        esteCoche.AgregarPuerta();
+        esteCoche.AgregarPuerta();
+        esteCoche.AgregarPuerta();
+        System.out.println("Puertas:" + esteCoche.puertas);
+    }
+
+
+
+
+    public static int suma (int a, int b, int c ){
+        return a + b +c;
+        }
+    static class  Coche {
+        public int puertas=0;
+        public void AgregarPuerta() {
+            puertas++;
+        }
+    }
+
+}
